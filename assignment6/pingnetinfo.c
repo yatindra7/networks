1db7051e8fb71db4860a2daebf60d09cfcef4686
srishtygandhi-kgp <101906706+srishtygandhi-kgp@users.noreply.github.com>
2023-03-31 22:43:07 +0530

    trace_route done

--- assignment6/pingnetinfo.c
+++ assignment6/pingnetinfo.c
@@ -15,20 +15,13 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-// PingNetInfo configuration
-#define MAX_HOPS 30 // Maximum number of hops to probe
-#define PACKET_SIZE 64 // ICMP packet size (in bytes)
-#define TIMEOUT_SEC 1 // Timeout for receiving ICMP response (in seconds)
-#define MAX_TRIES 5 // Maximum number of times to send a packet
-#define MAX_WAIT_MS 5000 // Maximum wait time (in milliseconds) for response to arrive
-#define DELAY_MS 1000 // Delay between packets (in milliseconds)
-#define BITS_PER_BYTE 8
+#include <ifaddrs.h>
+#include <netinet/tcp.h>
 
-// ICMP packet types
-#define ICMP_ECHO_REQUEST 8
-#define ICMP_ECHO_REPLY 0
-#define ICMP_TIME_EXCEEDED 11
-
+#define MAX_TTL 30         // sanity
+#define SOCKET_TIMEOUT 10  // in seconds
+#define RECV_BUF_LEN 10000
+#define PORT 20000
 // Structure to hold information about a network node
 typedef struct {
     char ip_address[16]; // IP address (in dotted decimal notation)
@@ -41,31 +34,13 @@ typedef struct {
 
 // Function prototypes
 
-static int GetSocket(void);
 static uint16_t Checksum(void* buf, int len); // tested
-static void ReverseDNSLookup(const char* ip_address, char* host_name, int host_name_size);
-static int SendICMPPacket(int socket_fd, const char* dest_address, int ttl, int sequence_number, int packet_size);
+struct sockaddr_in* getSrcAddr(); // tested
+struct in_addr * DNS_lookup(char* URL); // tested
 static double EstimateLatency(const struct timeval* send_time, const struct timeval* recv_time); // tested
 static double EstimateBandwidth(int* packet_sizes, double* latencies, int num_packets); // not sure about the formula
-static void PrintPacketDetails(const char* address, const struct sockaddr_in* from, int packet_size, int ttl, double latency, double bandwidth);
-static void PrintIPHeader(struct iphdr* ip_header);
-static void PrintTCPHeader(char* tcp_data);
-static void PrintUDPHeader(char* udp_data);
-static void PrintUnknownProtocolHeader();
 NodeInfo* PingNetInfo(const char* address, int* node_count);
 
-/**
- * Creates a raw socket and returns the file descriptor.
- * @return int - file descriptor of the raw socket
-*/
-static int GetSocket(void){
-    int socket_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    if (socket_fd < 0) {
-        perror("socket");
-        exit(1);
-    }
-    return socket_fd;
-}
 
 /**
  * Calculates the Internet Checksum of a given buffer of data.
@@ -75,8 +50,6 @@ static int GetSocket(void){
  * @param len   The size of the buffer of data.
  * @return       The 16-bit Internet Checksum of the given data.
  */
-
-
 static uint16_t Checksum(void* buf, int len) {
     uint16_t* buffer = buf;
     uint32_t sum = 0;
@@ -110,82 +83,60 @@ void test_checksum(){
      uint16_t buf[] = {0x4500, 0x003c, 0x1a2b, 0x0000, 0x8001, 0xb227, 0x0a00, 0x0001, 0x0a00, 0x0002};
     int len = sizeof(buf);
     uint16_t checksum = Checksum(buf, len);
-
     printf("Checksum: 0x%04x\n", checksum);
 }
 
 
-// not tested
 /**
-Given an IP address, performs a reverse DNS lookup to find the corresponding hostname.
-* @param ip_address const char* - IP address in string format
-* @param host_name char* - pointer to a character buffer where the hostname will be stored
-* @param host_name_size: int - size of the character buffer
-* @return void
-*/
-void ReverseDNSLookup(const char* ip_address, char* host_name, int host_name_size) {
-    struct sockaddr_in sa;  
-    socklen_t len = sizeof(sa); 
-    char ip[INET_ADDRSTRLEN]; // This creates a character array ip with a size of INET_ADDRSTRLEN (which is a constant defined in arpa/inet.h), which will be used to store the IP address in string form
-
-    // Convert IP address from string to binary form and store it in the sockaddr_in struct
-    inet_pton(AF_INET, ip_address, &(sa.sin_addr)); // This converts the IP address from string form (ip_address) to binary form, and stores it in the sin_addr field of sa. The AF_INET argument specifies that the IP address is in IPv4 format
-
-    // Use getnameinfo to perform reverse DNS lookup on the IP address and store the result in host_name
-    if (getnameinfo((struct sockaddr*)&sa, len, host_name, host_name_size, NULL, 0, 0) != 0) {  // This calls the getnameinfo function to perform the reverse DNS lookup. The first argument is a pointer to a sockaddr struct (which we cast to a sockaddr*), containing the IP address to look up. The second argument is the size of the sockaddr struct (len). The third argument is a pointer to the character array host_name, which will store the result of the lookup. The fourth argument is the size of host_name. The last three arguments are set to 0, indicating that we don't need any additional flags or services. The if statement checks whether getnameinfo returns a non-zero value, indicating that there was an error with the lookup
-        strncpy(host_name, ip_address, host_name_size - 1); // This copies the IP address from ip_address to host_name, in case the reverse DNS lookup failed. The host_name_size - 1 argument ensures that we don't overflow the host_name buffer
+ * Finds IP address of current interface
+ *
+ * @return  src_addr
+ */
+struct sockaddr_in* getSrcAddr() {
+    struct ifaddrs *id, *id_tmp;
+    struct sockaddr_in *src_addr = NULL;
+    int getAddress = getifaddrs(&id);
+    if (getAddress == -1)
+    {
+        perror("Unable to retrieve IP address of interface");
+        exit(-1);
     }
+
+    id_tmp = id;
+    while (id_tmp) 
+    {
+        if ((id_tmp->ifa_addr) && (id_tmp->ifa_addr->sa_family == AF_INET))
+        {
+            src_addr = (struct sockaddr_in *) id_tmp->ifa_addr;
+            break;
+        }
+        id_tmp = id_tmp->ifa_next;
+    }
+
+    if (src_addr == NULL) {
+        perror("Unable to find IP address of interface");
+        exit(-1);
+    }
+
+    return src_addr;
 }
 
-// not tested
 /**
-* Sends an ICMP packet to the specified destination address with the given TTL and sequence number.
-* @param socket_fd: int - file descriptor of the raw socket
-* @param dest_address: const char* - destination IP address in string format
-* @param ttl: int - TTL value for the ICMP packet
-* @param sequence_number: int - sequence number for the ICMP packet
-* @param packet_size: int - size of the ICMP packet to be sent
-* @return int - number of bytes sent, or -1 if an error occurred
-*/
-static int SendICMPPacket(int socket_fd, const char* dest_address, int ttl, int sequence_number, int packet_size) {
-    // initialize a sockaddr_in struct dest_addr
-    struct sockaddr_in dest_addr;
-    memset(&dest_addr, 0, sizeof(dest_addr));
-    dest_addr.sin_family = AF_INET;
-    dest_addr.sin_addr.s_addr = inet_addr(dest_address);
-
-    struct icmphdr icmp_header;
-    icmp_header.type = ICMP_ECHO;
-    icmp_header.code = 0;
-    icmp_header.checksum = 0;
-    icmp_header.un.echo.id = htons(getpid() & 0xFFFF);
-    icmp_header.un.echo.sequence = htons(sequence_number);
-    
-    char* icmp_packet = (char*) malloc(packet_size);
-    memset(icmp_packet, 0, packet_size);
-    memcpy(icmp_packet, &icmp_header, sizeof(struct icmphdr));
-    memset(icmp_packet + sizeof(struct icmphdr), 'x', packet_size - sizeof(struct icmphdr));
-    ((struct icmphdr*) icmp_packet)->checksum = Checksum(icmp_packet, packet_size);
-    
-    struct timeval send_time;
-    gettimeofday(&send_time, NULL);
-    
-    if (setsockopt(socket_fd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) != 0) {
-        fprintf(stderr, "Error setting TTL value: %s\n", strerror(errno));
-        return -1;
-    }
-    
-    int bytes_sent = sendto(socket_fd, icmp_packet, packet_size, 0, (struct sockaddr*)&dest_addr, sizeof(dest_addr));
-    if (bytes_sent <= 0) {
-        fprintf(stderr, "Error sending packet: %s\n", strerror(errno));
-        free(icmp_packet);
-        return -1;
-    }
-    
-    free(icmp_packet);
-    return 0;
+ * Finds out IP address of the given URL
+ *
+ * @param URL  -- URL (destination to be traced)
+ * @return  IP address of the given URL
+ */
+struct in_addr * DNS_lookup(char* URL){
+    struct hostent *host;
+        host = gethostbyname(URL);
+        if (host == NULL)
+        {
+            perror("Invalid host or failed DNS resolution");
+            exit(-1);
+        }
+    return (struct in_addr *)host->h_addr;
 }
-
 
 /**
 * Given the send and receive times of an ICMP packet, calculates the latency in milliseconds.
@@ -240,43 +191,6 @@ void test_estimated_bandwidth(){
     printf("Estimated bandwidth: %.2f Mbps\n", bandwidth);
 }
 
-/**
-* Prints the details of the received ICMP packet.
-* @param address: const char* - IP address in string format of the intermediate node
-* @param from: const struct sockaddr_in* - pointer to the sockaddr_in struct representing the source address of the received packet
-* @param packet_size: int - size of the received packet in bytes
-* @param ttl: int - TTL value of the received packet
-* @param latency: double - latency in milliseconds of the received packet
-* @param bandwidth: double - estimated bandwidth in bits per second of the received packet
-* @return void
-*/
-static void PrintPacketDetails(const char* address, const struct sockaddr_in* from, int packet_size, int ttl, double latency, double bandwidth);
-
-/**
- * Prints the IP header fields.
- *
- * @param ip_header The pointer to the IP header structure.
- */
-static void PrintIPHeader(struct iphdr* ip_header);
-
-/**
- * Prints the TCP header fields.
- *
- * @param tcp_data The pointer to the TCP data.
- */
-static void PrintTCPHeader(char* tcp_data);
-
-/**
- * Prints the UDP header fields.
- *
- * @param udp_data The pointer to the UDP data.
- */
-static void PrintUDPHeader(char* udp_data);
-
-/**
- * Prints a message indicating that the protocol is unknown.
- */
-static void PrintUnknownProtocolHeader();
 
 /**
  * Performs a traceroute to the specified address, estimates the latency and bandwidth of each
@@ -299,7 +213,123 @@ NodeInfo* PingNetInfo(const char* address, int* node_count);
  * @return     An integer indicating the exit status of the program.
  */
 
-int main(){
+int main(int argc, char *argv[]){
+
+    if (argc < 4)
+    {
+        printf("incomplete arguments\n ");
+        exit(-1);
+    }
+
+    char *trace_dest = argv[1]; //  a site to probe (can be either a name like cse.iitkgp.ac.in or an IP address like 10.3.45.6)
+    int num_probes = atoi(argv[2]); // the number of times a probe will be sent per link
+    int T = atoi(argv[3]); // time difference between any two probes
+
+    struct sockaddr_in *src_addr = getSrcAddr();
+    printf("PingNetInfo from host: %s\n", inet_ntoa(src_addr->sin_addr));
+
+    struct sockaddr_in dest_addr; 
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = htons(PORT);
+
+    int is_ip_addr = inet_aton(trace_dest, &(dest_addr.sin_addr)); // returns zero if trace dest is invalid
+    if (!is_ip_addr)
+    {
+        dest_addr.sin_addr = *((struct in_addr *)DNS_lookup(trace_dest));
+        printf("[DNS resolution] %s resolved to %s\n",trace_dest,inet_ntoa(dest_addr.sin_addr));
+    }
+
+    
+    // create socket to send tcp messages
+    int sendSocket = socket(PF_INET, SOCK_STREAM, 0);
+    if (sendSocket < 0){perror("Error creating tcp socket");exit(-1);}
+
+    // create socket to receive icmp messages
+    int recvSocket = socket(PF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (recvSocket < 0){perror("Error creating icmp socket");exit(-1);}
+
+    char recvBuffer[RECV_BUF_LEN];
+    struct sockaddr_in cli_addr;
+    socklen_t cli_len = sizeof(struct sockaddr_in);
+    long numBytesReceived;
+
+    printf("--------------- PingNetInfo results ---------------\n");
+    printf("0: %s [start]\n", inet_ntoa(src_addr->sin_addr));
+    int i = 0;
+    while (i < MAX_TTL)
+    {
+        i++;
+
+        // set TTL in IP header
+        setsockopt(sendSocket, IPPROTO_IP, IP_TTL, &i, sizeof(i));
+
+        errno = 0;
+        connect(sendSocket, (struct sockaddr *)&dest_addr, sizeof(struct sockaddr));
+
+        int icmpErrorReceived = 0;
+
+        // TTL expired
+        if (errno == EHOSTUNREACH)
+        {
+            while (!icmpErrorReceived)
+            {
+                // do in loop
+                numBytesReceived = recvfrom(recvSocket,recvBuffer,RECV_BUF_LEN,0,(struct sockaddr *)&cli_addr,&cli_len);
+
+                // extract IP header
+                struct ip *ip_hdr = (struct ip *)recvBuffer;
+
+                // extract ICMP header
+                int ipHeaderLength = 4 * ip_hdr->ip_hl;
+                struct icmp *icmp_hdr = (struct icmp *)( (char*) ip_hdr + ipHeaderLength );
+
+                int icmpMessageType = icmp_hdr->icmp_type;
+                int icmpMessageCode = icmp_hdr->icmp_code;
+
+                // TTL exceeded
+                if (icmpMessageType == ICMP_TIME_EXCEEDED && icmpMessageCode == ICMP_NET_UNREACH)
+                {
+                    // check if ICMP messages are related to TCP packets
+                    struct ip *inner_ip_hdr = (struct ip *)( (char*) icmp_hdr + ICMP_MINLEN);
+                    if (inner_ip_hdr->ip_p == IPPROTO_TCP)
+                    {icmpErrorReceived = 1;}
+                }
+
+                // port unreachable
+                else if (icmpMessageType == ICMP_DEST_UNREACH && icmpMessageCode == ICMP_PORT_UNREACH)
+                {
+                    printf("%d: %s [complete]\n", i, inet_ntoa(dest_addr.sin_addr));
+                    printf("--------------- PingNetInfo terminated ---------------\n");
+                    exit(0);
+                }
+            }
+            printf("%d: %s\n", i, inet_ntoa(cli_addr.sin_addr));
+
+            // success full so send it 5 tines
+
+            // case: timeout
+        } else if ( errno == ETIMEDOUT      // socket timeout
+                || errno == EINPROGRESS // operation in progress
+                || errno == EALREADY    // consecutive timeouts
+                )
+        {printf("%d: * * * * * [timeout]\n", i);}
+
+        // case: destination reached
+        else if (errno == ECONNRESET || errno == ECONNREFUSED)
+        {
+            printf("%d: %s [complete]\n",i, inet_ntoa(dest_addr.sin_addr));
+            printf("--------------- PingNetInfo terminated ---------------\n");
+            exit(0);
+        }
+        else
+        {
+            printf("Unknown error: %d sending packet\n", errno);
+            exit(-1);
+        }
+    }
+    printf("Unable to reach host within TTL of %d\n", MAX_TTL); 
+    printf("--------------- PingNetInfo terminated ---------------\n");
+    return -1;
     test_checksum();
     test_estimated_latency();
     test_estimated_bandwidth();
